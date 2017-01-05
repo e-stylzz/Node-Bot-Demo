ab53076617c56e76e868b56e1fbf66aa32d5a5fb
Eric Barb <eric.barb@gmail.com>
2017-01-05 12:54:01 -0500

    Fun

--- app.js
+++ app.js
@@ -17,16 +17,48 @@ server.get(/.*/, restify.serveStatic({
 
 // Create the bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID || config.id,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || config.password
 });
+
 var bot = new builder.UniversalBot(connector);
+//var bot = new builder.TextBot();
+
 server.post('/api/messages', connector.listen());
 
 
 // Bot Response Stuff
+// bot.dialog('/', function (session) {
+//     session.send("Hello World");
+// });
+
 bot.dialog('/', function (session) {
-    session.send("Hello World");
+    if (!session.userData.name) {
+        session.beginDialog('/profile');
+    } else {
+        session.send('Hello %s!', session.userData.name);
+        session.beginDialog('/whatsup');
+    }
 });
 
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi!  What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+        session.beginDialog('/whatsup');
+    }
+]);
 
+bot.dialog('/whatsup', [
+    function (session) {
+        builder.Prompts.text(session, 'What can I do for you ' + session.userData.name + '?');
+    },
+    function (session, results) {
+        session.userData.help = results.response;
+        session.endDialog();
+        session.send('So you need help with ' + session.userData.help + '?  Piss Off!');
+    }
+]);
