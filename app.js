2499a9bdfa9324a1f500411ee49d32891be26a58
Eric Barb <eric.barb@gmail.com>
2017-01-05 12:20:40 -0500

    Home Page

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+// Server Stuff
 var server = restify.createServer();
-
 server.listen(process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
+
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+	'directory': '.',
+	'default': 'index.html'
+}));
+
 
 // Create the bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
-
 var bot = new builder.UniversalBot(connector);
-
 server.post('/api/messages', connector.listen());
 
 
+// Bot Response Stuff
 bot.dialog('/', function (session) {
-    sessionsend("Hello World");
+    session.send("Hello World");
 });
 
 
